70517432788ea26bd76861759584f5984de544d4
MUlytskyi <w67969@student.wsiz.edu.pl>
2023-04-09 21:17:43 +0200

    Lab4

--- Laboratorium_Prog/script.js
+++ Laboratorium_Prog/script.js
@@ -27,11 +27,12 @@ function NWD(a, b) {
 }
 
 let s = 0;
+let x = 0;
+let y = 0;
 let numbers1 = '';
 let list_nums = [];
 let list_opers = [];
 let F = [];
-
 
 function display(a) {
     document.getElementById("wynik_kalk").value += a;
@@ -50,37 +51,133 @@ function calculate() {
     list_nums.push(numbers1);
     numbers1 = '';
     F = Cleaning(list_nums);
-    let obl = [];
-    for (let x in list_opers) {
-        if (list_opers[x] === '**') {
-            let k = (F[x]);
-            let r = (F[x + 1]);
-             s = k ** r;
-             F[x] = s;
-           // F[x + 1] = '';
-           // F = Cleaning(F);
+    for (y in list_opers) {
+        if (list_opers[y] === '**') {
+            x = y;
+            let x1 = ++x;
+            --x;
+            let k = F[x];
+            let r = (F[x1]);
+            s = parseFloat(k) ** parseFloat(r);
+            F[x] = s;
+            F[x1] = '';
+            F = Cleaning(F);
+            list_opers[y] = '';
+            list_opers = Cleaning_opers(list_opers);
         } 
-    } 
-    console.log(F);
-    console.log(list_opers);
-    console.log(parseFloat(s))
+    }
+    for (y in list_opers) {
+        if (list_opers[y] === '*') {
+            x = y;
+            let x1 = ++x;
+            --x;
+            let k = F[x];
+            let r = (F[x1]);
+            s = parseFloat(k) * parseFloat(r);
+            F[x] = s;
+            F[x1] = '';
+            F = Cleaning(F);
+            list_opers[y] = '';
+            list_opers = Cleaning_opers(list_opers);
+        }
+        if (list_opers.length > y && list_opers[y] === '/') {
+            x = y;
+            let x1 = ++x;
+            --x;
+            let k = F[x];
+            let r = (F[x1]);
+            s = parseFloat(k) / parseFloat(r);
+            F[x] = s;
+            F[x1] = '';
+            F = Cleaning(F);
+            list_opers[y] = ''
+            list_opers = Cleaning_opers(list_opers);
+        }
+    }  
+    for (y in list_opers) {
+        if (list_opers[y] === '-') {
+            x = y;
+            let x1 = ++x;
+            --x;
+            let k = F[x];
+            let r = (F[x1]);
+            s = parseFloat(k) - parseFloat(r);
+            F[x] = s;
+            F[x1] = '';
+            F = Cleaning(F);
+            list_opers[y] = '';
+            list_opers = Cleaning_opers(list_opers);
+        }
+        if (list_opers[y] === '+') {
+            x = y;
+            let x1 = ++x;
+            --x;
+            let k = F[x];
+            let r = (F[x1]);
+            s = parseFloat(k) + parseFloat(r);
+            F[x] = s;
+            F[x1] = '';
+            F = Cleaning(F);
+            list_opers[y] = '';
+            list_opers = Cleaning_opers(list_opers);
+        }
+    }
+    display(` = ${s}`);
 }
 
 
 function clearScreen() {
     list_nums.length = 0;
     list_opers.length = 0;
+    numbers1 = 0;
+    F.length = 0;
+    x = '';
+    y = '';
+    s = '';
     document.getElementById("wynik_kalk").value = "";
 }
 
 
 function Cleaning(nums) {
-    let D = [];
-    for (let i in nums) {
-        if (nums[i] != '') {
+    let D = [];  
+    for (let i in nums) { 
+        if (nums[i] != '') { 
             D.push(nums[i]);
         }
     }
-    F = D;
+    F = D; 
     return F;
 }
+function Cleaning_opers(oper) {
+    let Op = [];
+    for (let j in oper) {
+        if (oper[j] != '') {
+            Op.push(oper[j]);
+        }
+    }
+    list_opers = Op;
+    return list_opers;
+}
+
+/* На вход приходит отсортированный масив чисел. Нужно найти любую пару чисел которые будут равны загаданой сумме или вывести что результатов нет
+let D = [-5, -1, 2, 4, 7, 8, 10, 15];
+let a = 0;
+let b = 0;
+let i1 = 0;
+let i2 = D.length-1;
+let x_wyn = 0;
+while (i1 <= i2) {
+    a = D[i1];
+    b = D[i2];
+    if (parseFloat(a) + parseFloat(b) > 16) {
+        i2--;
+    } else if (parseFloat(a) + parseFloat(b) < 16) {
+        i1++
+    } else {
+        x_wyn = parseFloat(a) + parseFloat(b);
+        console.log(`Wynik: a = ${a}, + b = ${b} = ${x_wyn}`);
+        break;
+    }
+}
+if (i2 > i1) {console.log("Ничего не найдено");} 
+*/
