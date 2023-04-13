d7ae19df1d9436f6a4ec5d9b2d037617815d5728
MUlytskyi <w67969@student.wsiz.edu.pl>
2023-04-13 20:28:52 +0200

    Lab5

---
+++ Laboratorium_Prog/Lab5/lab_5(bank).js
@@ -0,0 +1,29 @@
+﻿const users = {
+    "0": {
+        "imie": "Matvii",
+        "nazwisko": "Ulytskyi",
+        "pin": "1234",
+        "pieniadzy": 100
+    },
+    "1": {
+        "imie": "Maria",
+        "nazwisko": "Korol",
+        "pin": "0000",
+        "pieniadzy": 50
+    }
+}
+
+
+function log() {
+    let login = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    if (users[login] && users[login].pin == pin) {
+        window.location.href = "Bank.html";
+        const user = users[login];
+        const obj = JSON.parse(user);
+        document.getElementById("Info").innerHTML = `Dzień dobry ${obj.imie} ${obj.nazwisko}, \n Pan/Pani posiada: ${obj.pieniadzy} zł.`;
+    }
+    else {
+        alert("Niepoprawny login lub password");
+    }
+};
---
+++ Laboratorium_Prog/Lab5/script_lab5.js
@@ -0,0 +1,63 @@
+﻿for (let i = 0; i <= 10; i++) {
+    const p = document.createElement("p");
+    if (i === 0) {
+        p.textContent = `0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 |`; 
+    } else {
+        p.textContent = `${i} | ${i * 1} | ${i * 2} | ${i * 3} | ${i * 4} | ${i * 5} | ${i * 6} | ${i * 7} | ${i * 8} | ${i * 9} | ${i * 10} |`;
+    }
+    document.getElementById("tabliczka_mnozenia").appendChild(p);
+}
+
+
+
+const names = ['Matvii', 'Bogdan', 'John', 'Oleksandr', 'Victoria', 'Pawel'];
+const surnames = ['Ulytskyi', 'Mamskii', 'Jozef', 'Brydko', 'Luchko', 'Chan'];
+
+
+function DaneOsob() {
+    const name = names[Math.floor(Math.random() * names.length)];
+    const surname = surnames[Math.floor(Math.random() * surnames.length)];
+    const age = Math.floor(Math.random() * 63) + 18;
+    const phone = Math.floor(Math.random() * 3000000) + 5000000;
+    return { name, surname, age, phone };
+}
+
+const ludzi = [];
+for (let i = 0; i < 20; i++) {
+    ludzi.push(DaneOsob());
+}
+
+let count = parseInt(prompt("Podaj ilość rekordów: "));
+console.log(ludzi.slice(0, count));
+
+ludzi.map(item => {
+    let div = document.createElement('div');
+    div.innerHTML += `<p>${item.name}</p>`;
+    div.innerHTML += `<p>${item.surname}</p>`;
+    div.innerHTML += `<p>${item.age}</p>`;
+    div.innerHTML += `<p>${item.phone}</p>`;
+    document.getElementById("Wyswetlanie").appendChild(div);
+});
+
+const people = [];
+const output = document.getElementById("Wyswetlanie");
+people.forEach(persona => {
+    const informacje = document.createElement('div');
+    const nameBox = document.createElement('p');
+    const surnameBox = document.createElement('p');
+    const ageBox = document.createElement('p');
+    const phoneBox = document.createElement('p');
+    const nameText = document.createTextNode("Name" + persona.name);
+    const surnameText = document.createTextNode("Surname" + persona.surname);
+    const ageText = document.createTextNode("Age" + persona.age);
+    const phoneText = document.createTextNode("Phone" + persona.phone);
+    nameBox.appendChild(nameText);
+    surnameBox.appendChild(surnameText);
+    ageBox.appendChild(ageText);
+    phoneBox.appendChild(phoneText);
+    informacje.appendChild(nameBox);
+    informacje.appendChild(surnameBox);
+    informacje.appendChild(ageBox);
+    informacje.appendChild(phoneBox);
+    output.appendChild(informacje);
+});
