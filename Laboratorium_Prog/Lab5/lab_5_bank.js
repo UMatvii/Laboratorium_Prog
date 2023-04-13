1cf41cc6d91a87a365288aa91c2d8173d4e540c2
MUlytskyi <w67969@student.wsiz.edu.pl>
2023-04-14 00:20:15 +0200

    Lab5

--- Laboratorium_Prog/Lab5/lab_5_bank.js
+++ Laboratorium_Prog/Lab5/lab_5_bank.js
@@ -1,29 +1,25 @@
-﻿const users = {
-    "0": {
-        "imie": "Matvii",
-        "nazwisko": "Ulytskyi",
-        "pin": "1234",
-        "pieniadzy": 100
-    },
-    "1": {
-        "imie": "Maria",
-        "nazwisko": "Korol",
-        "pin": "0000",
-        "pieniadzy": 50
-    }
-}
-
+﻿const users = '{"0": {"imie":"Matvii", "nazwisko": "Ulytskyi", "pin": "1234", "pieniadzy": 100}, "1": { "imie": "Maria", "nazwisko": "Korol", "pin": "0000", "pieniadzy": 50 } }';
 
 function log() {
+    user_obj = JSON.parse(users);
     let login = document.getElementById("username").value;
     let password = document.getElementById("password").value;
-    if (users[login] && users[login].pin == pin) {
-        window.location.href = "Bank.html";
-        const user = users[login];
-        const obj = JSON.parse(user);
-        document.getElementById("Info").innerHTML = `Dzień dobry ${obj.imie} ${obj.nazwisko}, \n Pan/Pani posiada: ${obj.pieniadzy} zł.`;
+   
+    document.getElementById("test").innerHTML = user_obj[login].pin;
+    if (user_obj[login].pin == password) {
+        console.log(`user_obj[login] = ${user_obj[login]}`);
+        log_no = login;
+        console.log(`log_no = ${log_no}`);
+        window.location.href = "Bank.html?" + login;
     }
     else {
         alert("Niepoprawny login lub password");
     }
 };
+function user_info() {
+    var log_no = location.search.substring(1);
+    console.log(`log_no = ${log_no}`);
+    user_obj = JSON.parse(users);
+    document.getElementById("Info").innerHTML = `Dzień dobry ${user_obj[log_no].imie} ${user_obj[log_no].nazwisko}, \n Pan/Pani posiada: ${user_obj[log_no].pieniadzy} zł.`
+    
+} 
